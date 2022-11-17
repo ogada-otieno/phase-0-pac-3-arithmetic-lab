abf25347c2bdf1a6d6eee19ebeddbb38bf011c6f
Rodgers Ogada <ogadarodgers@gmail.com>
2022-11-17 15:09:36 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a, b) {
+    let sum = a + b;
+    return sum;
+}
+
+function subtract(a, b) {
+    let sub = a - b;
+    return sub;
+}
+
+function multiply(a, b) {
+    let product = a * b;
+    return product; 
+}
+
+function divide(a, b) {
+    let division = a / b;
+    return division;
+}
+
+function increment(n) {
+    n+= 1;
+    return n;
+}
+
+function decrement(n) {
+    n -= 1;
+    return n;
+}
+
+function makeInt(n) {
+    return parseFloat(n);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
